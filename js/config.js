a4e5dcf1cfa181eefc163858870aa06c013c0f1a
Dan Kerimdzhanov <kerimdzhanov@gmail.com>
2022-09-25 21:41:47 +0100

    Update Cognito configs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_OuZU5SFcP',
+        userPoolClientId: '3biacq1pklo530r9k3q5n8q0fg',
+        region: 'eu-west-1' // Dublin, Ireland
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
